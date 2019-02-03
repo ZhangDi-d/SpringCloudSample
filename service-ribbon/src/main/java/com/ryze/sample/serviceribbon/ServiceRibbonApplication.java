9dfae4b043cb26f59e1179aae6e0a127f996755f
ZhangDi-d <1772085788@qq.com>
2019-02-03 11:25:19 +0800

    Feign 服务的消费方式有两种:1.RestTemplate+Ribbon;2.Feign去消费服务

---
+++ service-ribbon/src/main/java/com/ryze/sample/serviceribbon/ServiceRibbonApplication.java
@@ -0,0 +1,37 @@
+package com.ryze.sample.serviceribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 使用了＠EnableDiscoveryClient 注解来修改启动类，该注解使得服务调用
+ * 者有能力去 Eureka 中发现服务。需要注意的是，＠EnableEurekaClient 注解己经包含了
+ * @EnableDiscoveryClient 的功能，也就是说，一个 Eureka 客户端，本身就具有发现服务 的
+ * 能力
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class ServiceRibbonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceRibbonApplication.class, args);
+    }
+
+    /**
+     * 向程序的ioc注入一个bean: restTemplate;并通过@LoadBalanced注解表明这个restRemplate开启负载均衡的功能
+     * @return
+     */
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
+
