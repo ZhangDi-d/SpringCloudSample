914156b8a5398ad0b5e545df808754d4bab85402
ZhangDi-d <1772085788@qq.com>
2019-02-16 15:09:27 +0800

    zuul

--- README.md
+++ README.md
@@ -9,7 +9,6 @@ A simple project of springcloud self-learning.
 Feign是一个声明式的伪Http客户端，它使得写Http客户端变得更简单。使用Feign，只需要创建一个接口并注解。它具有可插拔的注解特性，可使用Feign 注解和JAX-RS注解。Feign支持可插拔的编码器和解码器。Feign默认集成了Ribbon，并和Eureka结合，默认实现了负载均衡的效果。
 
 简而言之：
-
 1.Feign 采用的是基于接口的注解
 2.Feign 整合了ribbon，具有负载均衡的能力
 3.整合了Hystrix，具有熔断的能力
@@ -23,3 +22,7 @@ Feign是一个声明式的伪Http客户端，它使得写Http客户端变得更�
 
 2.feign中使用断路器:
 Feign是自带断路器的，在D版本的Spring Cloud之后，它没有默认打开.打开:feign.hystrix.enabled=true
+
+
+## zuul
+Zuul的主要功能是路由转发和过滤器。路由功能是微服务的一部分，比如／api/user转发到到user服务，/api/shop转发到到shop服务。zuul默认和Ribbon结合实现了负载均衡的功能。
---
+++ service-zuul/src/main/java/com/ryze/sample/servicezuul/ServiceZuulApplication.java
@@ -0,0 +1,20 @@
+package com.ryze.sample.servicezuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
+@EnableZuulProxy    //@EnableZuulProxy，开启zuul的功能
+public class ServiceZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceZuulApplication.class, args);
+    }
+
+}
+
